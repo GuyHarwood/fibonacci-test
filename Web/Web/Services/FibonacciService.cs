2f6bde1ad515c31a98cf53772f18f27bca475838
Marcel du Preez <marceldupr@gmail.com>
2015-06-17 14:41:21 +0100

    initial commit

---
+++ Web/Web/Services/FibonacciService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Services
+{
+    public class FibonacciService : IFibonacciService
+    {
+        public IEnumerable<int> GenerateFibonacciSequence(int numbersToGenerate)
+        {
+            // TODO: Have not done the implementation yet.
+
+            return new List<int>();
+        }
+    }
+}
